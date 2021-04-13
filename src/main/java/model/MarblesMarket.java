790364c4971c1c3b81cf7196ba2396872cd862b5
Pannuba <occhipinti99@disroot.org>
2021-04-13 13:51:43 +0200

    finished marble constructor, mostly

--- src/main/java/model/MarblesMarket.java
+++ src/main/java/model/MarblesMarket.java
@@ -1,6 +1,6 @@
 package model;
 
-/*	13 total marbles:
+/*	13 total marbles, 12 in matrix and 1 spare:
 	4 white, 2 blue, 2 grey, 2 yellow, 2 purple, 1 red
 
 	Hardcode or xml file for marble amounts? Need to change grid size if so
@@ -15,51 +15,88 @@ public class MarblesMarket
 	private Resource[][] marblesBoard = new Resource[3][4];
 	private Resource spareMarble;
 
-	public MarblesMarket()
+	public MarblesMarket()		/* TODO: tests */
 	{
-		int[] marblesToAssign = new int[5];
-		ResourceType[] resourcesToAssign = new ResourceType[5];
+		int[] marblesToAssign = {4, 2, 2, 2, 2, 1};
+		ResourceType[] resourcesToAssign = new ResourceType[13];
 
-		marblesToAssign[0] = 4;
-		marblesToAssign[1] = 2;
-		marblesToAssign[2] = 2;
-		marblesToAssign[3] = 2;
-		marblesToAssign[4] = 2;
-		marblesToAssign[5] = 1;
-
-		switch (ThreadLocalRandom.current().nextInt(0, 5))			/* fill resourcesToAssign[], maybe use a while. Last one is spareMarble */
+		for (int i = 0; i < 13; i++)
 		{
+			switch (ThreadLocalRandom.current().nextInt(0, 5+1))            /* 5+1 because bound is not included */
+			{
 				case 0:
 
 					if (marblesToAssign[0] == 0)
 					{
-						i--;	/* So it iterates another time, doesn't count because we ran out of white marbles */
+						i--;    /* So it iterates another time, doesn't count because we ran out of white marbles */
 						break;
 					}
+
 					marblesToAssign[0]--;
-					//resourceToAssign = ResourceType.WHITE;
+					//resourceToAssign[i] = ResourceType.WHITE;
 					break;
 
 				case 1:
+
+					if (marblesToAssign[1] == 0)
+					{
+						i--;
+						break;
+					}
 					marblesToAssign[1]--;
-					resourceToAssign = ResourceType.BLUE;
+					resourcesToAssign[i] = ResourceType.BLUE;
 					break;
 
 				case 2:
+
+					if (marblesToAssign[2] == 0)
+					{
+						i--;
+						break;
+					}
 					marblesToAssign[2]--;
-					// ...
+					resourcesToAssign[i] = ResourceType.GREY;
+					break;
 
 				case 3:
 
+					if (marblesToAssign[3] == 0)
+					{
+						i--;
+						break;
+					}
+					marblesToAssign[3]--;
+					resourcesToAssign[i] = ResourceType.PURPLE;
+					break;
+
 				case 4:
 
+					if (marblesToAssign[4] == 0)
+					{
+						i--;
+						break;
+					}
+					marblesToAssign[4]--;
+					resourcesToAssign[i] = ResourceType.YELLOW;
+					break;
+
 				case 5:
+
+					if (marblesToAssign[5] == 0)
+					{
+						i--;
+						break;
+					}
+					marblesToAssign[5]--;
+					resourcesToAssign[i] = ResourceType.RED;
+					break;
 
 				default:
 					break;
 			}
+		}
 
-		for (int i = 0; i < 13; i++)
+		for (int i = 0; i < 13;   )
 		{
 			if (i == 12)		/* TODO: test this in a separate program , should exit all for loops because the last marble to assign is the spare one */
 			{
@@ -73,6 +110,7 @@ public class MarblesMarket
 				{
 					marblesBoard[j][k] = new Resource();
 					marblesBoard[j][k].setResourceType(resourcesToAssign[i]);        /* Need to use MarbleType enum and Marble class */
+					i++;
 				}
 			}
 		}
