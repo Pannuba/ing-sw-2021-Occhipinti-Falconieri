601b7e5ae3f67357e59fc5df782f1d836453b4b2
Chiara <kiafal@libero.it>
2021-05-24 20:00:22 +0200

    GameState test

--- src/test/java/it/polimi/ingsw/model/board/StorageTest.java
+++ src/test/java/it/polimi/ingsw/model/board/StorageTest.java
@@ -13,6 +13,7 @@ public class StorageTest
     private Resource a = new Resource();
     private Resource b = new Resource();
     private Resource c = new Resource();
+    private Resource resourceToAdd = new Resource();
     private Shelf[] shelves = new Shelf[3];
 
 
