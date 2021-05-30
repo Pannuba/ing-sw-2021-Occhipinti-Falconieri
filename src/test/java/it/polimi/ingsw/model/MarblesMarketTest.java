4c92ae6692f7fcd9cb98d09473ab798a9b84acc7
Chiara <kiafal@libero.it>
2021-05-30 22:09:07 +0200

    MarbleMarket tests

---
+++ src/test/java/it/polimi/ingsw/model/MarblesMarketTest.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MarblesMarketTest {
+
+	private MarblesMarket marblesMarket;
+	private List<MarbleType> boughtMarbles = new ArrayList<>();
+	private Marble[][] marblesBoard = new Marble[3][4];
+	private Marble spareMarble;
+	private Marble oldSpareMarble;
+
+	@Test
+	public void buyMarblesRow()
+	{
+		marblesMarket = new MarblesMarket();
+		marblesBoard = marblesMarket.getMarblesBoard();
+		for (int i = 0; i < 4; i++)
+			boughtMarbles.add(marblesBoard[0][i].getMarbleType());
+		spareMarble = new Marble();
+		spareMarble = marblesBoard[0][0];
+		oldSpareMarble = new Marble();
+		oldSpareMarble = marblesMarket.getSpareMarble();
+		assertEquals("Error", boughtMarbles, marblesMarket.buyMarblesRow(0));
+		assertEquals("Error", spareMarble, marblesMarket.getSpareMarble());
+		assertEquals("Error", oldSpareMarble, marblesBoard[0][3]);
+	}
+
+	@Test
+	public void buyMarblesCol()
+	{
+		marblesMarket = new MarblesMarket();
+		marblesBoard = marblesMarket.getMarblesBoard();
+		for (int i = 0; i < 3; i++)
+			boughtMarbles.add(marblesBoard[i][3].getMarbleType());
+		spareMarble = new Marble();
+		spareMarble = marblesBoard[0][3];
+		oldSpareMarble = new Marble();
+		oldSpareMarble = marblesMarket.getSpareMarble();
+		assertEquals("Error", boughtMarbles, marblesMarket.buyMarblesCol(3));
+		assertEquals("Error", spareMarble, marblesMarket.getSpareMarble());
+		assertEquals("Error", oldSpareMarble, marblesBoard[2][3]);
+	}
+}
