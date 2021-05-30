9d66e14fa2418fe54d4ee77efecb4d66dbcd79e3
Chiara <kiafal@libero.it>
2021-05-30 19:46:41 +0200

    Cards tests: ActionBlack1Test, ActionBlack2Test

---
+++ src/test/java/it/polimi/ingsw/model/cards/ActionBlack1Test.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.model.cards;
+
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.board.Track;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ActionBlack1Test {
+
+	private ActionToken actionToken;
+	private List<ActionToken> actionTokens = new ArrayList<>();
+	private Track track;
+	private List<Player> players;
+	private Player p1;
+
+	@Test
+	public void doAction()
+	{
+		p1 = new Player("pippo");
+		players = new ArrayList<>();
+		players.add(p1);
+		track = new Track(players);
+		track.setBlackPawn(3);
+		actionToken = new ActionBlack1(track, actionTokens);
+		actionToken.doAction();
+		assertEquals("Error", 4, track.getBlackPawn());
+	}
+}
