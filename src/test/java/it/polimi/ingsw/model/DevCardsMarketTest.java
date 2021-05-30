e5135da9109a66e32ea275abf94314e26cb6220b
Chiara <kiafal@libero.it>
2021-05-30 18:50:12 +0200

    DevCardMarket test

---
+++ src/test/java/it/polimi/ingsw/model/DevCardsMarketTest.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.cards.DevCard;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DevCardsMarketTest {
+
+	private DevCardsMarket devCardsMarket;
+	private DevCard card;
+
+	@Test
+	public void buyCardFromMarket()
+	{
+		devCardsMarket = new DevCardsMarket();
+		card = new DevCard();
+		card = devCardsMarket.getDevCardByNumber(27);
+		assertEquals("Error", card, devCardsMarket.buyCardFromMarket(27));
+		card = devCardsMarket.getDevCardByNumber(30);
+		assertEquals("Error", card, devCardsMarket.buyCardFromMarket(30));
+		assertEquals("Error", null, devCardsMarket.getDevCardByNumber(27));  /* check that the card has really been removed */
+		assertEquals("Error", null, devCardsMarket.getDevCardByNumber(30));
+	}
+}
