446b8bc940ceb2bcfb4cdf373e355273d1518620
Pannuba <occhipinti99@disroot.org>
2021-05-31 15:40:34 +0200

    removed useless position field from Box

--- src/main/java/it/polimi/ingsw/model/board/Box.java
+++ src/main/java/it/polimi/ingsw/model/board/Box.java
@@ -5,24 +5,17 @@ import java.io.Serializable;
 public class Box implements Serializable
 {
 	private final BoxType boxType;
-	private final int position;
 	private final int victoryPoints;
 
-	public Box(BoxType boxType, int position, int victoryPoints)
+	public Box(BoxType boxType, int victoryPoints)
 	{
 		this.boxType = boxType;
-		this.position = position;
 		this.victoryPoints = victoryPoints;
 	}
 
 	public BoxType getBoxType()
 	{
 		return boxType;
-	}
-
-	public int getPosition()
-	{
-		return position;
 	}
 
 	public int getVictoryPoints()
--- src/main/java/it/polimi/ingsw/model/board/Track.java
+++ src/main/java/it/polimi/ingsw/model/board/Track.java
@@ -35,31 +35,31 @@ public class Track implements Serializable
 
 		/* Initialize track here, hardcoded (at least for now) because 1 xml for each box is too much */
 
-		faithTrack[0]  = new Box(BoxType.NORMAL, 0,  0);
-		faithTrack[1]  = new Box(BoxType.NORMAL, 1,  0);
-		faithTrack[2]  = new Box(BoxType.NORMAL, 2,  0);
-		faithTrack[3]  = new Box(BoxType.POINTS, 3,  1);
-		faithTrack[4]  = new Box(BoxType.NORMAL, 4,  0);
-		faithTrack[5]  = new Box(BoxType.NORMAL, 5,  0);
-		faithTrack[6]  = new Box(BoxType.POINTS, 6,  2);
-		faithTrack[7]  = new Box(BoxType.NORMAL, 7,  0);
-		faithTrack[8]  = new Box(BoxType.POPE,   8,  0);
-		faithTrack[9]  = new Box(BoxType.POINTS, 9,  4);
-		faithTrack[10] = new Box(BoxType.NORMAL, 10, 0);
-		faithTrack[11] = new Box(BoxType.NORMAL, 11, 0);
-		faithTrack[12] = new Box(BoxType.POINTS, 12, 6);
-		faithTrack[13] = new Box(BoxType.NORMAL, 13, 0);
-		faithTrack[14] = new Box(BoxType.NORMAL, 14, 0);
-		faithTrack[15] = new Box(BoxType.POINTS, 15, 9);
-		faithTrack[16] = new Box(BoxType.POPE,   16, 0);
-		faithTrack[17] = new Box(BoxType.NORMAL, 17, 0);
-		faithTrack[18] = new Box(BoxType.POINTS, 18, 12);
-		faithTrack[19] = new Box(BoxType.NORMAL, 19, 0);
-		faithTrack[20] = new Box(BoxType.NORMAL, 20, 0);
-		faithTrack[21] = new Box(BoxType.POINTS, 21, 16);
-		faithTrack[22] = new Box(BoxType.NORMAL, 22, 0);
-		faithTrack[23] = new Box(BoxType.NORMAL, 23, 0);
-		faithTrack[24] = new Box(BoxType.POPE,   24, 20);
+		faithTrack[0]  = new Box(BoxType.NORMAL, 0);
+		faithTrack[1]  = new Box(BoxType.NORMAL, 0);
+		faithTrack[2]  = new Box(BoxType.NORMAL, 0);
+		faithTrack[3]  = new Box(BoxType.POINTS, 1);
+		faithTrack[4]  = new Box(BoxType.NORMAL, 0);
+		faithTrack[5]  = new Box(BoxType.NORMAL, 0);
+		faithTrack[6]  = new Box(BoxType.POINTS, 2);
+		faithTrack[7]  = new Box(BoxType.NORMAL, 0);
+		faithTrack[8]  = new Box(BoxType.POPE,   0);
+		faithTrack[9]  = new Box(BoxType.POINTS, 4);
+		faithTrack[10] = new Box(BoxType.NORMAL, 0);
+		faithTrack[11] = new Box(BoxType.NORMAL, 0);
+		faithTrack[12] = new Box(BoxType.POINTS, 6);
+		faithTrack[13] = new Box(BoxType.NORMAL, 0);
+		faithTrack[14] = new Box(BoxType.NORMAL, 0);
+		faithTrack[15] = new Box(BoxType.POINTS, 9);
+		faithTrack[16] = new Box(BoxType.POPE,   0);
+		faithTrack[17] = new Box(BoxType.NORMAL, 0);
+		faithTrack[18] = new Box(BoxType.POINTS, 12);
+		faithTrack[19] = new Box(BoxType.NORMAL, 0);
+		faithTrack[20] = new Box(BoxType.NORMAL, 0);
+		faithTrack[21] = new Box(BoxType.POINTS, 16);
+		faithTrack[22] = new Box(BoxType.NORMAL, 0);
+		faithTrack[23] = new Box(BoxType.NORMAL, 0);
+		faithTrack[24] = new Box(BoxType.POPE,   20);
 	}
 
 	public int checkVaticanReport()		/* Returns the number of the pope box that triggered the vatican report. 0 if no vatican report */
