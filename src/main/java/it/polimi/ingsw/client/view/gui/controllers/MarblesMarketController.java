4cff5312faaf17949d96ae723d07420e12414670
Pannuba <occhipinti99@disroot.org>
2021-06-02 11:07:34 +0200

    finished marblesmarket scene, initial controller

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/MarblesMarketController.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.client.view.gui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class MarblesMarketController
+{
+
+	@FXML private ImageView marblesMarket;
+
+	@FXML private Button colOneButton;
+	@FXML private Button colTwoButton;
+	@FXML private Button colThreeButton;
+	@FXML private Button colFourButton;
+
+	@FXML private Button rowOneButton;
+	@FXML private Button rowThreeButton;
+	@FXML private Button rowFourButton;
+
+	@FXML private ImageView marble00;
+	@FXML private ImageView marble01;
+	@FXML private ImageView marble02;
+	@FXML private ImageView marble03;
+	@FXML private ImageView marble10;
+	@FXML private ImageView marble11;
+	@FXML private ImageView marble12;
+	@FXML private ImageView marble13;
+	@FXML private ImageView marble20;
+	@FXML private ImageView marble21;
+	@FXML private ImageView marble22;
+	@FXML private ImageView marble23;
+
+	@FXML private Button backToBoardButton;
+
+	@FXML
+	void backToBoard(ActionEvent event)
+	{
+
+	}
+
+}
