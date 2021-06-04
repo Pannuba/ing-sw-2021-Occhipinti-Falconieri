cfc50d6c8c6c41131a730838131152c669ee922b
Chiara <kiafal@libero.it>
2021-06-04 17:20:43 +0200

    leaderCardes scene

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/LeaderCardsController.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.client.view.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class LeaderCardsController {
+
+	@FXML
+	private ImageView leaderCard1;
+
+	@FXML
+	private ImageView leaderCard2;
+
+	@FXML
+	private Button playLeaderCard1;
+
+	@FXML
+	private Button discardLeaderCard1;
+
+	@FXML
+	private Button playLeaderCard2;
+
+	@FXML
+	private Button discardLeaderCard2;
+
+	@FXML
+	private Button backToBoard;
+
+}
