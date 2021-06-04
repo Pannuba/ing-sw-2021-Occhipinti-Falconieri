e44465d942f1935b49a6d283c6252374ec5d7933
Pannuba <occhipinti99@disroot.org>
2021-06-04 16:17:53 +0200

    added option to activate or discard a leader twice in the same round in CLI

--- src/main/java/it/polimi/ingsw/client/view/cli/actions/LeaderChoice.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/actions/LeaderChoice.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.client.view.cli.actions;
 
 import it.polimi.ingsw.client.NetworkHandler;
+import it.polimi.ingsw.client.localserver.LocalServer;
 import it.polimi.ingsw.client.view.cli.CLI;
 import it.polimi.ingsw.client.view.cli.PrintMethods;
 import it.polimi.ingsw.model.cards.LeaderCard;
@@ -16,23 +17,21 @@ public class LeaderChoice
 	public LeaderChoice(Scanner input, List<String> command, NetworkHandler networkHandler, CLI cli)
 	{
 		List<LeaderCard> leaderCards = cli.getGameState().getPlayerByName(cli.getUsername()).getLeaderCards();
-		List<LeaderCard> inactiveLeaders = new ArrayList<>();
+		List<LeaderCard> canBeActivated = new ArrayList<>();
+		List<LeaderCard> canBeDiscarded = new ArrayList<>();
 		String chosenCard;
 
 		for (int i = 0; i < leaderCards.size(); i++)
 		{
-			if (leaderCards.get(i).isDiscarded())
-			{
-				leaderCards.remove(i);		/* Discarded cards can't be activated nor discarded, so remove them */
-				i--;
-			}
-
 			if (!leaderCards.get(i).isActive())
-				inactiveLeaders.add(leaderCards.get(i));
+				canBeActivated.add(leaderCards.get(i));
+
+			if (!leaderCards.get(i).isDiscarded())
+				canBeDiscarded.add(leaderCards.get(i));
 		}
 
-		if (leaderCards.isEmpty())
-			return;			/* If the player has no leadercards or they are both discarded, skip the choice */
+		if (canBeActivated.isEmpty() && canBeDiscarded.isEmpty())
+			return;			/* If the player has no leaders that can be activated or discarded, skip the choice */
 
 		System.out.print("Do you want to activate or discard a leader? A/D: ");
 
@@ -40,14 +39,14 @@ public class LeaderChoice
 		{
 			case "A":		/* Only pass cards that can be activated */
 
-				if (inactiveLeaders.isEmpty())
+				if (canBeActivated.isEmpty())
 				{
 					System.out.println("You don't have any leaders that can be activated!");
 					return;
 				}
 
-				PrintMethods.printPlayerLeaderCards(inactiveLeaders);
-				System.out.print("Select card #" + inactiveLeaders.get(0).getCardNumber() + " or #" + inactiveLeaders.get(1).getCardNumber() + ": ");
+				PrintMethods.printPlayerLeaderCards(canBeActivated);
+				System.out.print("Select card #" + canBeActivated.get(0).getCardNumber() + " or #" + canBeActivated.get(1).getCardNumber() + ": ");
 				chosenCard = input.nextLine();
 
 				command.add("ACTIVATE_LEADER");
@@ -57,7 +56,14 @@ public class LeaderChoice
 				break;
 
 			case "D":		/* Only pass cards that can be discarded */
-				PrintMethods.printPlayerLeaderCards(leaderCards);
+
+				if (canBeDiscarded.isEmpty())
+				{
+					System.out.println("You don't have any leaders that can be discarded!");
+					return;
+				}
+
+				PrintMethods.printPlayerLeaderCards(canBeDiscarded);
 				System.out.print("Select card #" + leaderCards.get(0).getCardNumber() + " or #" + leaderCards.get(1).getCardNumber() + ": ");
 				chosenCard = input.nextLine();
 
@@ -66,6 +72,17 @@ public class LeaderChoice
 				networkHandler.send(command);
 				command.clear();
 				break;
+
+			default:
+				return;
+		}
+		/* - 1 because the player has just performed a successful action */
+		if ((canBeActivated.size() - 1) > 0 || (canBeDiscarded.size() - 1) > 0)		/* If there are still card to activate or discard, ask again */
+		{
+			System.out.print("Activate or discard another leader? (Y/N) ");
+
+			if (input.nextLine().equalsIgnoreCase("Y"))
+				new LeaderChoice(input, command, networkHandler, cli);
 		}
 	}
 }
