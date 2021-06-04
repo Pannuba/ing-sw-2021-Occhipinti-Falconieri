d90a0e848fadf7385ef03bd0e617c6c25e0aa117
Chiara <kiafal@libero.it>
2021-06-04 17:53:33 +0200

    endGame scene

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/EndGameController.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.client.view.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class EndGameController {
+
+	@FXML
+	private ImageView winOrLose;
+
+	@FXML
+	private Label winnerPoints;
+
+	@FXML
+	private Button playAgain;
+
+	@FXML
+	private Label secondPlacePoints;
+
+	@FXML
+	private Label thirdPlacePoints;
+
+	@FXML
+	private Label fourthPlacePoints;
+
+}
