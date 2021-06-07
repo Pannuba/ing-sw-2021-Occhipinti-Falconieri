d820a34acef1801efc6ef0bedac8a57e74a65db2
Chiara <kiafal@libero.it>
2021-06-07 11:42:19 +0200

    edit endgame fxml

--- src/main/java/it/polimi/ingsw/client/view/gui/controllers/EndGameController.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/EndGameController.java
@@ -1,20 +1,23 @@
 package it.polimi.ingsw.client.view.gui.controllers;
 
+import it.polimi.ingsw.client.view.gui.ActionGUI;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class EndGameController {
 
 	@FXML
-	private ImageView winOrLose;
+	private ImageView win;
 
 	@FXML
 	private Label winnerPoints;
-
-	@FXML
-	private Button playAgain;
 
 	@FXML
 	private Label secondPlacePoints;
@@ -25,4 +28,23 @@ public class EndGameController {
 	@FXML
 	private Label fourthPlacePoints;
 
+	@FXML
+	private ImageView playAgain;
+
+	@FXML
+	private ImageView lose;
+
+	@FXML
+	void playAgain(MouseEvent event)
+	{
+
+	}
+
+	public void setup(ActionGUI actionGUI) throws IOException {
+
+	}
+
+	public ImageView getWin() { return win; }
+
+	public ImageView getLose() { return lose; }
 }
