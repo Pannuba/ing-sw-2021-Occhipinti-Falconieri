da8c86846f0bd1c111213d78af9e15837b7e41f4
Pannuba <occhipinti99@disroot.org>
2021-06-29 23:23:44 +0200

    working custom font, tweaked scenes

--- src/main/java/it/polimi/ingsw/client/view/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/GUI.java
@@ -13,6 +13,7 @@ import it.polimi.ingsw.server.messages.Message;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
--- src/main/java/it/polimi/ingsw/client/view/gui/Launcher.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/Launcher.java
@@ -21,7 +21,7 @@ public class Launcher extends Application
 	@Override
 	public void start(Stage mainStage) throws IOException			/* Stage = window */
 	{
-		Font.loadFont(getClass().getResourceAsStream("/renaissance.ttf"), 15);
+		Font.loadFont(getClass().getResourceAsStream("/CharpentierRenRed-Normal.ttf"), 15);
 
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("/scenes/launcher.fxml"));
